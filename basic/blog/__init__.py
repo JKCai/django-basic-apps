3e88fab6cef3524d9e8d84dbb17dfb5906c82ed5
kevin fricovsky <kevin@montylounge.com>
2011-03-27 00:04:33 -0400

    removing more twitter integration

--- basic/blog/__init__.py
+++ basic/blog/__init__.py
@@ -5,4 +5,3 @@ from django.contrib.sites.models import Site
 
 signals.post_save.connect(update_settings, Site, True)
 signals.post_save.connect(invalidate_settings_cache, Settings, True)
-signals.pre_save.connect(post_to_twitter, Post)
